c00052a006f8745b41d873586aed442c7f4cae1b
obuyajr <felix37brock@gmail.com>
2023-07-10 14:06:14 +0300

    assign Houses

--- Forms/frmPayment_records.Designer.vb
+++ Forms/frmPayment_records.Designer.vb
@@ -26,6 +26,8 @@ Partial Class frmPayment_records
         Label1 = New Label()
         combo_houseNo = New ComboBox()
         Panel2 = New Panel()
+        TextBox1 = New TextBox()
+        Label5 = New Label()
         txt_id = New TextBox()
         Label3 = New Label()
         txt_balance = New TextBox()
@@ -33,8 +35,13 @@ Partial Class frmPayment_records
         Label4 = New Label()
         Label2 = New Label()
         Panel3 = New Panel()
+        Button1 = New Button()
+        txt_mMoney = New TextBox()
+        Label6 = New Label()
+        btn_cancel = New Button()
         Panel1.SuspendLayout()
         Panel2.SuspendLayout()
+        Panel3.SuspendLayout()
         SuspendLayout()
         ' 
         ' Panel1
@@ -71,6 +78,10 @@ Partial Class frmPayment_records
         ' 
         Panel2.BackColor = Color.White
         Panel2.BorderStyle = BorderStyle.Fixed3D
+        Panel2.Controls.Add(txt_mMoney)
+        Panel2.Controls.Add(Label6)
+        Panel2.Controls.Add(TextBox1)
+        Panel2.Controls.Add(Label5)
         Panel2.Controls.Add(txt_id)
         Panel2.Controls.Add(Label3)
         Panel2.Controls.Add(txt_balance)
@@ -79,8 +90,28 @@ Partial Class frmPayment_records
         Panel2.Controls.Add(Label2)
         Panel2.Location = New Point(12, 99)
         Panel2.Name = "Panel2"
-        Panel2.Size = New Size(723, 225)
+        Panel2.Size = New Size(723, 312)
         Panel2.TabIndex = 1
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Font = New Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point)
+        TextBox1.ForeColor = Color.FromArgb(CByte(0), CByte(0), CByte(192))
+        TextBox1.Location = New Point(133, 205)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(131, 43)
+        TextBox1.TabIndex = 14
+        TextBox1.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Label5.Location = New Point(46, 224)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(73, 17)
+        Label5.TabIndex = 13
+        Label5.Text = "CashPaid :"
         ' 
         ' txt_id
         ' 
@@ -145,17 +176,63 @@ Partial Class frmPayment_records
         ' 
         Panel3.BackColor = Color.White
         Panel3.BorderStyle = BorderStyle.FixedSingle
-        Panel3.Location = New Point(12, 375)
+        Panel3.Controls.Add(btn_cancel)
+        Panel3.Controls.Add(Button1)
+        Panel3.Location = New Point(12, 417)
         Panel3.Name = "Panel3"
-        Panel3.Size = New Size(723, 68)
+        Panel3.Size = New Size(723, 80)
         Panel3.TabIndex = 2
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI Black", 11.25F, FontStyle.Bold, GraphicsUnit.Point)
+        Button1.Image = My.Resources.Resources.wallet_regular_24
+        Button1.ImageAlign = ContentAlignment.MiddleRight
+        Button1.Location = New Point(93, 20)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(107, 32)
+        Button1.TabIndex = 0
+        Button1.Text = "Pay "
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' txt_mMoney
+        ' 
+        txt_mMoney.Font = New Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point)
+        txt_mMoney.ForeColor = Color.FromArgb(CByte(0), CByte(0), CByte(192))
+        txt_mMoney.Location = New Point(133, 156)
+        txt_mMoney.Name = "txt_mMoney"
+        txt_mMoney.Size = New Size(131, 43)
+        txt_mMoney.TabIndex = 16
+        txt_mMoney.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Label6.Location = New Point(8, 175)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(111, 17)
+        Label6.TabIndex = 15
+        Label6.Text = "M/ Money [F9] :"
+        ' 
+        ' btn_cancel
+        ' 
+        btn_cancel.Font = New Font("Segoe UI Black", 11.25F, FontStyle.Bold, GraphicsUnit.Point)
+        btn_cancel.Image = My.Resources.Resources.x_regular_24
+        btn_cancel.ImageAlign = ContentAlignment.MiddleRight
+        btn_cancel.Location = New Point(595, 23)
+        btn_cancel.Name = "btn_cancel"
+        btn_cancel.Size = New Size(107, 32)
+        btn_cancel.TabIndex = 2
+        btn_cancel.Text = "Cancel"
+        btn_cancel.UseVisualStyleBackColor = True
         ' 
         ' frmPayment_records
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         BackColor = Color.MistyRose
-        ClientSize = New Size(747, 481)
+        ClientSize = New Size(747, 500)
         Controls.Add(Panel3)
         Controls.Add(Panel2)
         Controls.Add(Panel1)
@@ -168,6 +245,7 @@ Partial Class frmPayment_records
         Panel1.PerformLayout()
         Panel2.ResumeLayout(False)
         Panel2.PerformLayout()
+        Panel3.ResumeLayout(False)
         ResumeLayout(False)
     End Sub
 
@@ -182,4 +260,10 @@ Partial Class frmPayment_records
     Friend WithEvents Panel3 As Panel
     Friend WithEvents txt_id As TextBox
     Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txt_mMoney As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents btn_cancel As Button
 End Class
--- My Project/Resources.Designer.vb
+++ My Project/Resources.Designer.vb
@@ -199,5 +199,25 @@ Namespace My.Resources
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property wallet_regular_24() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("wallet-regular-24", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property x_regular_24() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("x-regular-24", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
     End Module
 End Namespace
