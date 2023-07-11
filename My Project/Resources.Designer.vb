5a0a04f205d97f30b2c7ee714ba07f7cc32ff489
obuyajr <felix37brock@gmail.com>
2023-07-11 21:29:13 +0300

    payment

--- My Project/Resources.Designer.vb
+++ My Project/Resources.Designer.vb
@@ -193,6 +193,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property report_solid_24__1_() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("report-solid-24 (1)", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property right_arrow_solid_24() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("right-arrow-solid-24", resourceCulture)
